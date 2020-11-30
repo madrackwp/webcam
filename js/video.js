749378d50662548528851af73fceb658a85844bc
madrackwp <52032916+madrackwp@users.noreply.github.com>
2020-11-30 11:08:01 +0800

    I can access 2 different cameras and stream it to the web server

--- js/video.js
+++ js/video.js
@@ -1,19 +1,74 @@
+// (function(){
+//     var video = document.getElementById('video'),
+//         vendorUrl = window.URL || window.webkitURL;
+
+//     navigator.getMedia = navigator.getUserMedia ||
+//                          navigator.webkitGetUserMedia ||
+//                          navigator.mozGetUserMedia ||
+//                          navigator.msGetUserMedia;
+
+//     navigator.getMedia({
+//         video: true,
+//         audio: false
+//     }, function(stream){
+//         video.srcObject = stream;
+//         video.play();
+//     }, function(error){
+
+//     });
+// })();
+
+
 (function(){
-    var video = document.getElementById('video'),
-        vendorUrl = window.URL || windor.webkitURL;
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia){
+        console.log("getUserMedia() not supported");
+        return;
+    }
 
-    navigator.getMedia = navigator.getUserMedia ||
-                         navigator.webkitGetUserMedia ||
-                         navigator.mozGetUserMedia ||
-                         navigator.msGetUserMedia;
+    //This is to go through and log all the available cameras the browser has access to
+    navigator.mediaDevices.enumerateDevices() 
+          .then(function(devices) {
+            devices.forEach(function(device) {
+              console.log(device.kind + ": " + device.label +
+                          " id = " + device.deviceId);
+            });
+          })
+          .catch(function(err) {
+            console.log(err.name + ": " + err.message);
+          });
 
-    navigator.getMedia({
-        video: true,
-        audio: false
-    }, function(stream){
-        video.srcObject = stream;
-        video.play();
-    }, function(error){
+    navigator.mediaDevices.getUserMedia({ audio: false, 
+                                        video: {deviceId: "7a04b37385f557c505fc4462f9cb5eea7de2045a15810ba3b096e0e45329f826"} //deviceId will default to the default camera on the device
+                                        })
+    .then(function(stream){
+        var video = document.getElementById('video');
+        if ("srcObject" in video){
+            video.srcObject = stream;
+        } else {
+            video.src = window.URL.createObjectURL(stream);
+        }
+        video.onloadedmetadata = function(e){
+            video.play();
+        };
+    }).catch(function(err){
+        console.log(err.name + ": "+ err.message);
+    });
 
+    //This is to access the second camera so that it can be play on the video
+    navigator.mediaDevices.getUserMedia({ audio: false, 
+                                        video: {deviceId: "bceadf9ba7ee5cdf1c9d90980931bb7a961c00682011493748f8717cfa287b83"} //deviceId will default to the default camera on the device
+                                        })
+    .then(function(stream){
+        var video = document.getElementById('video2');
+        if ("srcObject" in video){
+            video.srcObject = stream;
+        } else {
+            video.src = window.URL.createObjectURL(stream);
+        }
+        video.onloadedmetadata = function(e){
+            video.play();
+        };
+    }).catch(function(err){
+        console.log(err.name + ": "+ err.message);
     });
 })();
